7acc69e7d24ef2791b307c3a286f6c8858bd5e1c
Rob Hitt <robhitt@gmail.com>
2017-04-02 22:35:56 -0400

    Upload for content for blog

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var firstId = document.getElementById('first');
-var greenID = document.getElementById("Second");
+var mainId = document.getElementById('main');
 
-firstId.innerHTML = "robert hitt"; 
-
+var firstId = document.getElementById('click-handler');
 firstId.addEventListener('click', clickMe);
+var fifthId = document.getElementById('button-click');
+fifthId.addEventListener('click', clickMe);
 
 function clickMe(event) {
-  console.log("clicked");
-  firstId.innerHTML = 'Goodbye';
+  mainId.innerHTML = 'You Clicked Me!';
+}
+
+
+var secondId = document.getElementById("mouse-over-handler");
+secondId.addEventListener('mouseover', mouseOverMe);
+
+function mouseOverMe(event) {
+  mainId.innerHTML = 'You Moused Over!';
+}
+
+
+var thirdId = document.getElementById("pop-up-handler");
+thirdId.addEventListener('click', popMe);
+
+function popMe(event) {
+  mainId.innerHTML = 'Popped It!';
+  alert("Nice pop!");
+}
+
+
+var fourthId = document.getElementById("change-me")
+fourthId.addEventListener('input', changeMe);
+
+function changeMe(event) {
+  console.log(event.target.value)
+  mainId.innerHTML = event.target.value
 }
